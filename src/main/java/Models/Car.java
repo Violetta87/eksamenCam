631bbfe1866a967ab630d6fab178796b640abfee
Violetta87 <athena_mewo87@hotmail.com>
2022-05-16 12:45:48 +0200

    new

--- src/main/java/Models/Car.java
+++ src/main/java/Models/Car.java
@@ -1,5 +1,63 @@
 package Models;
 
 public class Car {
+String model;
+String brand;
+int carbonEmission;
+int registrationCost;
+String equipmentLevel;
+boolean isRented;
+boolean isDamaged;
 
+public Car(String model,String brand, int carbonEmission, int registrationCost, String equipmentLevel, boolean isRented, boolean isDamaged){
+    this.model=model;
+    this.brand=brand;
+    this.carbonEmission=carbonEmission;
+    this.registrationCost=registrationCost;
+    this.equipmentLevel=equipmentLevel;
+    this.isRented=isRented;
+    this.isDamaged=isDamaged;
 }
+
+    public void setModel(String model) {this.model = model;}
+
+    public void setBrand(String brand) {this.brand = brand;}
+
+    public void setCarbonEmission(int carbonEmission) {this.carbonEmission = carbonEmission;}
+
+    public void setRegistrationCost(int registrationCost) {this.registrationCost = registrationCost;}
+
+    public void setEquipmentLevel(String equipmentLevel) {this.equipmentLevel = equipmentLevel;}
+
+    public void setRented(boolean rented) {isRented = rented;}
+
+    public void setDamaged(boolean damaged) {isDamaged = damaged;}
+
+    public String getModel() {return model;}
+
+    public String getBrand() {return brand;}
+
+    public int getCarbonEmission() {return carbonEmission;}
+
+    public int getRegistrationCost() {return registrationCost;}
+
+    public String getEquipmentLevel() {return equipmentLevel;}
+
+    public boolean isRented() {return isRented;}
+
+    public boolean isDamaged() {return isDamaged;}
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", brand='" + brand + '\'' +
+                ", carbonEmission=" + carbonEmission +
+                ", registrationCost=" + registrationCost +
+                ", equipmentLevel='" + equipmentLevel + '\'' +
+                ", isRented=" + isRented +
+                ", isDamaged=" + isDamaged +
+                '}';
+    }
+}
+
--- src/main/java/com/example/demo/Controllers/LeaseController.java
+++ src/main/java/com/example/demo/Controllers/LeaseController.java
@@ -1,12 +1,34 @@
 package com.example.demo.Controllers;
 
+import Models.Car;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
- @Controller
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.context.request.WebRequest;
+
+@Controller
 public class LeaseController {
 
         @GetMapping("/addLeasePage")
         public String addLeasePage(){
                 return "addLeasePage";
         }
+
+        @GetMapping("/addCars")
+        public String addCars(){ return "addCars";}
+
+         /*@PostMapping("/addCars")
+         public String registrerCars(WebRequest dataFromForm){
+                String model = dataFromForm.getParameter("model");
+                String brand = dataFromForm.getParameter("brand");
+                String carEmissionCost = dataFromForm.getParameter("carEmissionCost");
+                String registrationCost = dataFromForm.getParameter("registrationCost");
+                String equipmentLevel = dataFromForm.getParameter("equipmentLevel");
+                String isRented = dataFromForm.getParameter("isRented");
+                String isDamaged = dataFromForm.getParameter("isDamaged");
+
+             Car newCar = new Car(model, brand, carEmissionCost,registrationCost, equipmentLevel, isRented,isDamaged);
+
+         }*/
+
 }
