d73a31db1517bbf020130e2a9c18319dac594124
Violetta87 <athena_mewo87@hotmail.com>
2022-05-17 10:26:29 +0200

    fixet struktur

--- src/main/java/com/example/Controllers/LeaseController.java
+++ src/main/java/com/example/Controllers/LeaseController.java
@@ -1,7 +1,10 @@
 package com.example.Controllers;
 
+import com.example.Models.Car;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.context.request.WebRequest;
 
 @Controller
 public class LeaseController {
@@ -14,7 +17,7 @@ public class LeaseController {
         @GetMapping("/addCars")
         public String addCars(){ return "add-cars";}
 
-         /*@PostMapping("/addCars")
+         @PostMapping("/addCars")
          public String registrerCars(WebRequest dataFromForm){
                 String model = dataFromForm.getParameter("model");
                 String brand = dataFromForm.getParameter("brand");
